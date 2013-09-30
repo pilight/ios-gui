1d5c4d28ffa1208b6bcd9ddd9b3a7b6d8f12d607
Martin Kollaard <martin@Kollaard.eu>
2013-09-30 22:19:54 +0200

    Initial Commit

---
+++ pilight/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  pilight
+//
+//  Created by Martin Kollaard on 30-09-13.
+//  Copyright (c) 2013 Martin Kollaard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ pilight/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  pilight
+//
+//  Created by Martin Kollaard on 30-09-13.
+//  Copyright (c) 2013 Martin Kollaard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
